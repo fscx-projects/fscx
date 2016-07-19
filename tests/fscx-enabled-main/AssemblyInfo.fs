b5672e5e5dde032e13e780e7e544195ea30f9baa
Kouji Matsui <k@kekyo.net>
2016-07-19 11:51:02 +0900

    Validated can set break point in fscx'd project.

---
+++ tests/fscx-enabled-main/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace fscx_enabled_main.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリに関する一般情報は、以下の属性セットによって
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[<assembly: AssemblyTitle("fscx-enabled-main")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("fscx-enabled-main")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// ComVisible を false に設定すると、COM コンポーネントがこのアセンブリ内のその型を認識
+// できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[<assembly: ComVisible(false)>]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります
+[<assembly: Guid("f12ce420-d232-4dd2-80f2-82d6152b4292")>]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。:
+// 
+//       メジャー バージョン
+//       マイナー バージョン 
+//       ビルド番号
+//       リビジョン
+// 
+// すべての値を指定するか、下に示すように '*' を使用してビルドおよびリビジョン番号を
+// 既定値にすることができます。:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ tests/fscx-enabled-main/Program.fs
@@ -0,0 +1,9 @@
+﻿// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+[<EntryPoint>]
+let main argv = 
+  FscxOutputSample1.f1 (123, "ABC", 456)
+  FscxOutputSample2.f2 (789, "DEF", 111)
+  printfn "%A" argv
+  0 // 整数の終了コードを返します
