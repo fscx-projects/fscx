e70f517d5d316eaf50e03459fddefc052e83e6aa
Kouji Matsui <k@kekyo.net>
2016-07-21 09:38:16 +0900

    Add no-fscx-enabled projects.  refs #46693

---
+++ tests/fscx-enabled/no-fscx-enabled-main/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.Runtime.InteropServices
+
+[<DllImport("user32.dll", CharSet = CharSet.Unicode)>]
+extern int MessageBox(IntPtr hWnd, string text, string caption, int options);
+
+[<EntryPoint>]
+let main argv = 
+  //MessageBox(IntPtr.Zero, "Wait for attached debugger...", "fscx-enabled-main", 0 ||| 0x30) |> ignore
+  FscxOutputSample1.f1 (123, "ABC", 456)
+  FscxOutputSample2.f2 (789, "DEF", 111)
+  printfn "%A" argv
+  0
---
+++ tests/fscx-enabled/no-fscx-enabled/FscxOutputSample2.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+//namespace FscxOutputSample
+
+module FscxOutputSample2
+
+  let f2 (a: int, b: string, c: int) =
+    System.Console.WriteLine("Sample2: {0}:{1}", a, b)
