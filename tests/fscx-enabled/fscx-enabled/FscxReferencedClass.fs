cf21d9d4f1ee5784feba2ccba4a50fb379aff861
Kouji Matsui <k@kekyo.net>
2016-10-06 14:18:43 +0900

    Remove sample projects.

--- tests/fscx-enabled/fscx-enabled/FscxReferencedClass.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sample
-
-type FscxReferencedClass() =
-
-  member __.F1 a b c =
-    FscxOutputSample1.f1(a, b, c)
-  member __.F2 a b c =
-    FscxOutputSample2.f2(a, b, c)
--- tests/fscx-enabled/fscx-enabled/test_msbuild.cmd
+++
@@ -1,2 +0,0 @@
-msbuild fscx-enabled.sln /p:Configuration=Debug /p:Platform="Any CPU"
-msbuild fscx-enabled.sln /p:Configuration=Release /p:Platform="Any CPU"
